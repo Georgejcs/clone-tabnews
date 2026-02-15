d85934fc4265f4bd5f97f953462f76322cc3829a
George José <118679416+Georgejcs@users.noreply.github.com>
2026-02-15 14:27:11 +0000

    add services scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Uma frase muito legal</h1>;
 }
 
-function teste() {
-  return console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
