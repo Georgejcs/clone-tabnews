626ce5291eb14eee0c00a88660a85488e2111cde
George José <118679416+Georgejcs@users.noreply.github.com>
2026-02-28 01:15:16 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,41 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Aluno do curso .dev acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW SERVER_VERSION");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+  const databaseConnectionsValue = databaseConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+
+    //database_version_value: databaseVersionValue,
+    // database_max_conections_value: databaseMaxConectionsValue,
+    // opened_connections_value: openedConnectionsValue,
+
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        database_connections: databaseConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
